d6637a3ae9519d3f70b00c48efa26ea23936c293
jihee <esder1310@naver.com>
2024-02-19 09:08:05 +0900

    0219 study add code

---
+++ react/studyproject/src/components/Home.js
@@ -0,0 +1,14 @@
+import React,{useState} from "react";
+import styled from "styled-components"
+
+import photo from "../asset/img/photo.jpg"
+
+function Home(){
+
+    return (
+        <>
+        </>
+    )
+}
+
+export default Home;
