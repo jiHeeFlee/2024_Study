044da0c401cef29d0ed8afcb047e96153ef03f3c
jihee <esder1310@naver.com>
2024-02-20 02:22:21 +0900

    0220study make component

---
+++ react/studyproject/src/components/Intro.js
@@ -0,0 +1,21 @@
+import React,{useState} from "react";
+import styled from "styled-components"
+
+function Intro(){
+
+    return(
+        <>
+            <Container>
+                intro !!!
+            </Container>
+        </>
+    )
+}
+
+export default Intro;
+
+const Container=styled.div`
+  display: flex;
+  flex-direction: column;
+  
+`;
