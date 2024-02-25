014958d7c989a4e0f9ee8b5424fd879756d51461
jihee <esder1310@naver.com>
2024-02-25 14:31:03 +0900

    0225study input tag code edit / css edit

--- react/studyproject/src/components/Input.js
+++ react/studyproject/src/components/Input.js
@@ -6,16 +6,20 @@ function Input(){
     return (
         <>
             <Container>
-                input이 왜안되는거야?
-                {/*<input*/}
-                {/*    type={'text'}*/}
-                {/*    placeholder={'please your message!'}*/}
-                {/*    className={'inputDiv'}*/}
-                {/*/>*/}
+                <input
+                    type={'text'}
+                    placeholder={'please write your message!'}
+                    className={'inputDiv'}
+                />
                 {/*<input*/}
                 {/*    type={'submit'}*/}
                 {/*    className={'inputSubmitBtn'}*/}
                 {/*>submit</input>*/}
+                <button
+                    type={'submit'}
+                    className={'inputSubmitBtn'}>
+                    submit
+                </button>
             </Container>
         </>
     )
@@ -27,21 +31,30 @@ const Container=styled.div`
   display: flex;
   flex-direction: row;
   
+  align-items: center;
   text-align: center;
   
   color: #2e2e2e;
   
   .inputDiv{
-    padding-left: 1vw;
-    
-    outline: none;
+    margin: 1vw;
+    padding: 0.5vw;
+
     border: #7f7f7f;
   }
   
   .inputSubmitBtn{
-    background-color: #fff4c2;
+    width: 4vw;
+    height: 1.3vh;
+    padding: 0.5%;
     
+    background-color: #fff4c2;
+    outline: none;
+    border: none;
+    //border-radius: 10%;
     &:hover{
+      background-color: orangered;
+      color: white;
       font-weight: 700;
     }
     
