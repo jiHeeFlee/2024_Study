bfe4bc73c508129dd2f50b8143b5ba2189c589e9
jihee <esder1310@naver.com>
2024-05-01 17:50:26 +0900

    react 폴더 삭제 후 react-redux 설치

--- react/studyproject/src/components/Welcome.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import styled from "@emotion/styled";
-import ReactDOM from "react-dom/client";
-
-function Welcome(props){
-    return <h1>Hello, {props.name}</h1>;
-}
-
-export default Welcome
